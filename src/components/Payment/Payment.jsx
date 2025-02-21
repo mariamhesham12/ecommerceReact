976c3b081e77836930b4ef31e4439ca3c45191cc
mariamhesham12 <99356287+mariamhesham12@users.noreply.github.com>
2025-02-22 01:28:39 +0200

    second update for the url

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -43,7 +43,7 @@ export default function Payment() {
                     token:localStorage.getItem("token")
                 },
                 params:{
-                    url:"https://mariamhesham12.github.io/"
+                    url:"http://localhost:5173/ecommerceReact"
                 }
             })
             toast.success(data.status)
